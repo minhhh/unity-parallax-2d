7f3a5192580dc91dfa31cbc423af87148d1aa4c0
Ha.Minh <minhhh@minhhuyha.info>
2016-02-02 21:13:38 +0700

    Add initial code

---
+++ Assets/ParallaxLayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ParallaxLayer : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+    public bool moveInOppositeDirection;
+    public bool moveParallax = true;
+
+    private Transform cameraTransform;
+    private Vector3 previousCameraPosition;
+
+    void OnEnable ()
+    {
+        GameObject gameCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+        cameraTransform = gameCamera.transform;
+        previousCameraPosition = cameraTransform.position;
+    }
+
+    void LateUpdate ()
+    {
+        if (!moveParallax)
+            return;
+
+        Vector3 distance = cameraTransform.position - previousCameraPosition;
+        float direction = (moveInOppositeDirection) ? -1f : 1f;
+        transform.position += Vector3.Scale (distance, new Vector3 (speedX, speedY)) * direction;
+
+        previousCameraPosition = cameraTransform.position;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # unity-parallax-2d
-Script for creating 2d parallax layer in Unity
+Simple script for creating 2d parallax layer in Unity. This is modified from the script in [this article](http://www.gamasutra.com/blogs/DavidDionPaquet/20140601/218766/Creating_a_parallax_system_in_Unity3D_is_harder_than_it_seems.php)
+
+## Usage
+Just attach the `ParallaxLayer` script to the specific layer that you want to move relatively to the main camera. Then you can set speed in X and Y direction from 0 to 1. The farther the layer is, the higher the speed. So a layer at infinity will have speed of 1, while the closest layer will have speed of 0.
+
+There's a `TestScene` for an example of how this work.
+
